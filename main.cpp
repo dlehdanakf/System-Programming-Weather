27d07c0691e1cbe96a551a6f1947f335c9eb51d8
LEE Dong Hyun <dlehdanakf@konkuk.ac.kr>
2016-11-01 12:00:02 +0900

    사용자 이름 입력받는거 구현

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-#include <cstdlib>
+#include <errno.h>
+#include <unistd.h>
+#include <string>
+
+#define BLKSIZE 1024
+
 using namespace std;
 
+void getUserNameFromKeyboard(char* buf, int &bytesread){
+    while (((bytesread = read(STDIN_FILENO, buf, BLKSIZE)) == -1) && (errno == EINTR)) ; /* handle interruption by signal */
+}
+
 int main(void){
-    cout << "이동현님 안녕하세요! 오늘 날씨 : 맑음, 26.5 *C | 한강수온 : 16 *C | 현재시각 : 23시 29분 43초" << endl;
+
+    int name_read;
+    int name_write;
+    char name_bf[BLKSIZE];
+    char welcome[] = {"님 안녕하세요"};
+
+    getUserNameFromKeyboard(name_bf, name_read);
+
+    while(((name_write = write(STDOUT_FILENO, name_bf, name_read - 1)) == -1 ) && (errno == EINTR)) ;
+    while(((name_write = write(STDOUT_FILENO, welcome, sizeof(welcome) - 1)) == -1 ) && (errno == EINTR)) ;
+
 }
